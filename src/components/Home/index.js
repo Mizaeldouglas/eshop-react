40eb5403ea30897afb669816ad8c06cc95464c9e
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-09-05 21:48:05 -0300

    home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,18 @@ import "./styles.css";
 
 export default function Home() {
 
-	return <h1>Hello World!!</h1>
+	return (
+		<div>
+			<div className="home">
+				<div className="home__container">
+					<img src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg" alt="Logo" className="home__image" />
+				
+				</div>
+					<div className="home__row"></div>
+
+
+			</div>
+		</div>
+	)
 
 }
